90e3b7c8e06b1c464322f2b3955805874ddd5e7c
Sean <sean.t.irby@gmail.com>
2013-05-28 11:18:53 -0700

    Change users admin default attribute to false

--- db/schema.rb
+++ db/schema.rb
@@ -71,8 +71,8 @@ ActiveRecord::Schema.define(:version => 20130528222120) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -81,10 +81,10 @@ ActiveRecord::Schema.define(:version => 20130528222120) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.integer  "partner_id"
-    t.boolean  "admin"
+    t.boolean  "admin",                  :default => false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
--- spec/features/admin/viewing_professionals_spec.rb
+++ spec/features/admin/viewing_professionals_spec.rb
@@ -4,7 +4,7 @@ feature 'viewing professionals' do
   let!(:user) { FactoryGirl.create(:confirmed_user) }
   let!(:admin) { FactoryGirl.create(:admin_user) }
 
-  scenario "A signed in admin user is redirected to partners index" do
+  scenario "A signed in admin user is redirected to partners index upon signing in" do
     visit '/users/sign_in'
     fill_in "Email", :with => admin.email
     fill_in "Password", :with => admin.password
