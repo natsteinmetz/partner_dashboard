c43b1559460ff67f5acfdb5d8007de4dd259e1de
James Adney <jfadney@gmail.com>
2013-05-28 13:00:22 -0700

    failing spec for viewing students

--- spec/features/viewing_students_spec.rb
+++ spec/features/viewing_students_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 feature "Viewing students" do
-  scenario "viewing students on students index"
+  let!(:student_albert) { FactoryGirl.create(:student,
+                                             name: "Albert Einstein",
+                                             skills: "relativity") }
+  let!(:student_werner) { FactoryGirl.create(:student,
+                                             name: "Werner Heisenberg",
+                                             skills: "quantum mechanics") }
+
+  before do
+    user = FactoryGirl.create(:user)
+    sign_in_as!(user)
+  end
+
+  scenario "viewing students on students index" do
+    visit "/"
+
+    page.should have_content(student_albert.name)
+    page.should have_content(student_albert.skills)
+
+    page.should have_content(student_werner.name)
+    page.should have_content(student_werner.skills)
+  end
 end
