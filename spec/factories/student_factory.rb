ab9aa84b8fe8832aa308886d50226e52d2a4c67d
James Adney <jfadney@gmail.com>
2013-05-28 15:37:52 -0700

    student factory saves phone number as string

--- spec/factories/student_factory.rb
+++ spec/factories/student_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :student do
     name "Jane Doe"
     email "student@example.com"
-    phone_number 112342312
+    phone_number "(555) 555-5555"
     skills "javascript, c++, ruby"
   end
 end
