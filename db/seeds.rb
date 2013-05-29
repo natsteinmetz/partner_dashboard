b1f42a6d946597fd44fbbe7972b59fa76acd4200
Sean <sean.t.irby@gmail.com>
2013-05-29 12:01:15 -0700

    Remove extraneous destroys from seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-#Destroy everything
-resources = ["Employment", "Mentorship", "Relationship", "Partner", "User", "Professional", "Student"]
+# #Destroy everything
+# resources = ["Employment", "Mentorship", "Relationship", "Partner", "User", "Professional", "Student"]
 
-resources.each do |resource|
-  Object.const_get(resource).destroy_all
-end
+# resources.each do |resource|
+#   Object.const_get(resource).destroy_all
+# end
 
 #Create an admin user
 admin = User.create(email: "admin@codefellows.com", password: "password")
