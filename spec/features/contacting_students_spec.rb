7c328158a7d0623525ff7048dd5b4798a2ce97e1
James Adney <jfadney@gmail.com>
2013-05-29 15:20:50 -0700

    add pending spec: users see who they have contacted

--- spec/features/contacting_students_spec.rb
+++ spec/features/contacting_students_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 feature "Contacting students" do
   scenario "requesting contact with a particular student"
+  scenario "users see which students they have requested contact with"
 end
