8f4b2b02c0117b8401e6e2e8a1f93b32e4624efa
James Adney <jfadney@gmail.com>
2013-05-29 14:40:08 -0700

    limit viewing students to signed-in users

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,6 @@
 class StudentsController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @students = Student.order("name")
   end
--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe StudentsController do
   context "no user signed in" do
-    it "does not show students to non-users"
+    it "does not show students to non-users" do
+      get :index
+      response.should redirect_to new_user_session_url
+      flash[:alert].should eql("You need to sign in or sign up before continuing.")
+    end
   end
 end
