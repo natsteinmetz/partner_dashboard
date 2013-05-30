f3d9565316966007682f749811b2a3247e00b595
Sean <sean.t.irby@gmail.com>
2013-05-30 10:58:00 -0700

    Add feature allowing admin to send people invitations and assign them to partners

--- spec/features/admin/admin_can_invite_potential_users_spec.rb
+++ spec/features/admin/admin_can_invite_potential_users_spec.rb
@@ -1,24 +1,39 @@
 require 'spec_helper'
 
-feature "admin can invite potential users" do
+feature "Inviting potential users" do
+  invitee_name = "invitee"
+  invitee_email = "invitee@example.com"
+  invitee_password = "password"
   let!(:user) { FactoryGirl.create(:confirmed_user) }
   let!(:admin) { FactoryGirl.create(:admin_user) }
+  let!(:partner) { FactoryGirl.create(:partner) }
 
-  scenario "admin can access the admin invitation" do
+  scenario "admin can send a user an invitation and assign him/her to a particular partner" do
+    #in context of admin
     sign_in_as!(admin)
     click_link "Invite A New User"
+    select partner.name, :from => "user[partner_id]"
+    fill_in "Email", :with => invitee_email
+    click_button "Send an invitation"
+
+    #TODO, this is ugly, Need to sign out as admin or i'll get redirected when I accept the invitation,
+    click_link "Sign out"
+
+    #in context of invited user
+    open_email "invitee@example.com", :with_subject => /Invitation instructions/
+    visit_in_email "Accept invitation"
+    puts page.body
+    fill_in "Password", :with => invitee_password
+    fill_in "Password confirmation", :with => invitee_password
+    click_button "Set my password"
+    invited_user = User.find_by_email(invitee_email)
+    assert(partner == invited_user.partner, "The invited user is not associated with the correct partner.
+                                             User's partner is : #{invited_user.partner.name}
+                                             and the test partner is: #{partner.name}")
   end
 
-  scenario "normal user cannot access the admin invitation" do
-  end
+  #TODO, I think this scenario needs to be moved into the invitations controller spec
+  scenario "normal user cannot send an invitation"
 
-  scenario "admin can select an existing partner in invitation form" do
-  end
-
-  scenario "admin can create a brand new partner in an invitation form" do
-  end
-
-  scenario "admin can send an invitation" do
-  end
-
+  scenario "admin can create a brand new partner in an invitation form"
 end
