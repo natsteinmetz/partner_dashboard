4de14f28ae1db72af3bdfac6f40bce4d3bc8d1d4
Sean <sean.t.irby@gmail.com>
2013-05-30 15:29:34 -0700

    Fix errors

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -7,9 +7,9 @@ feature "Root page" do
   scenario "Visiting root page shows an informative sign in page" do
     #Test for sign in form
     visit '/'
-    page.should have_link("Sign in")
+    page.should have_button("Sign in")
     #Test for link to request admin invite
-    page.should have_link("Request invitation")
+    page.should have_link("Request Invitation")
   end
 
   scenario "Root page has links to other codefellows sites and the about page"
