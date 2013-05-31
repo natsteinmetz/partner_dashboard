134a4c90118d62baff4c611592732e214c81e783
Sean <sean.t.irby@gmail.com>
2013-05-31 09:21:05 -0700

    Add filter feature to partners and professionals pages

--- spec/features/admin/viewing_partners_spec.rb
+++ spec/features/admin/viewing_partners_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 feature "Viewing Partners" do
-  let!(:partner_one) { FactoryGirl.create(:partner) }
-  let!(:partner_two) { FactoryGirl.create(:partner) }
+  let!(:partner_one) { FactoryGirl.create(:partner, kind: "BigCo") }
+  let!(:partner_two) { FactoryGirl.create(:partner, kind: "Startup") }
 
   context "without logging in" do
     scenario "Partners link is hidden"
@@ -26,6 +26,17 @@ feature "Viewing Partners" do
       page.should have_content partner_two.name
       page.should have_content partner_two.kind
     end
+
+    scenario "only showing partners of a particular type", :js => true do
+      sign_in_as!(admin)
+      visit "/"
+      click_link "Partners"
+      fill_in "partner-filter", with: partner_one.kind
+      page.should have_content partner_one.kind
+      page.should_not have_content partner_two.kind
+    end
+
+    scenario "admin can sort partners"
   end
 
 end
--- spec/features/admin/viewing_professionals_spec.rb
+++ spec/features/admin/viewing_professionals_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 feature 'viewing professionals' do
   let!(:user) { FactoryGirl.create(:confirmed_user) }
   let!(:admin) { FactoryGirl.create(:admin_user) }
-  let!(:professional_one) { FactoryGirl.create(:partner_with_professional).professionals.first }
-  let!(:professional_two) { FactoryGirl.create(:partner_with_professional).professionals.first }
-  let!(:professional_three) { FactoryGirl.create(:partner_with_professional).professionals.first }
+  let!(:professional_one) { FactoryGirl.create(:partner_with_professional, name: "Acme", kind: "BigCo").professionals.first }
+  let!(:professional_two) { FactoryGirl.create(:partner_with_professional, name: "Facebook", kind: "Startup").professionals.first }
 
   scenario "An admin user is redirected to professionals index upon signing in" do
     visit '/users/sign_in'
@@ -20,8 +19,7 @@ feature 'viewing professionals' do
     visit '/admin/professionals'
     page.should have_content "Code Fellows Company Professionals"
     [professional_one,
-     professional_two,
-     professional_three].each do |professional|
+     professional_two].each do |professional|
 
       page.should have_content(professional.name)
 
@@ -31,4 +29,16 @@ feature 'viewing professionals' do
 
     end
   end
+
+  scenario "admin user can filter professionals by partner name", :js => true do
+    sign_in_as!(admin)
+    visit '/'
+    click_link "Professionals"
+    fill_in "professional-filter", with: professional_one.partners.first.name
+    puts page.body
+    page.should have_content professional_one.name
+    page.should_not have_content professional_two.name
+  end
+
+  scenario "admin can sort professionals by ____"
 end
