62879afaaa249e2ee045816fd2e46f4888b14529
James Adney <jfadney@gmail.com>
2013-05-31 07:42:19 -0700

    pending specs to make sure non-admins can't see partners

---
+++ spec/controllers/partners_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe PartnersController do
+  context "no user signed in" do
+    it "does not show partners"
+  end
+
+  context "normal user signed in" do
+    it "does not show partners"
+  end
+end
