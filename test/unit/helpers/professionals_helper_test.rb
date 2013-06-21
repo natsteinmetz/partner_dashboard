83e1c196535815d961e9ea9602243d39b0482142
James Adney <jfadney@gmail.com>
2013-06-21 14:41:20 -0700

    remove testunit files

--- test/unit/helpers/professionals_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ProfessionalsHelperTest < ActionView::TestCase
-end
--- test/unit/professional_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProfessionalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
