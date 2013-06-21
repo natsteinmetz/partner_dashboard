cf8a5afd01712f5faa13db5d0ba20486837efdc1
James Adney <jfadney@gmail.com>
2013-06-20 21:32:53 -0700

    implement pending specs for partners controller

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -4,8 +4,8 @@ describe StudentsController do
   context "no user signed in" do
     it "does not show students to non-users" do
       get :index
-      response.should redirect_to new_user_session_url
-      flash[:alert].should eql("You need to sign in or sign up before continuing.")
+      response.should redirect_to(new_user_session_path)
+      flash[:alert].should eql(I18n.t "devise.failure.unauthenticated")
     end
   end
 end
