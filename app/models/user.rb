899d254eddf7e8f463e8ddf41c5373a4eef18d07
Sean <sean.t.irby@gmail.com>
2013-06-23 14:51:30 -0700

    Remove boilerplate comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,12 @@ class User < ActiveRecord::Base
 
   validates :partner, presence: true, unless: :admin?
 
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+
   devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :partner_id
-  # attr_accessible :title, :body
 
   def has_contacted?(student)
     if self.admin?
