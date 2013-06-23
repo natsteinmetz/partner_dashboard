d8ccd11d727448e09c54a58c387a6a4b60d6cc0e
Sean <sean.t.irby@gmail.com>
2013-06-23 14:37:23 -0700

    Add actions for creating a new professional in invitation form

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -15,10 +15,10 @@ class Admin::InvitationsController < Devise::InvitationsController
       #TODO: create a filter method to strip out unwanted params
       # so I can avoid user_params nastiness above
       build_resource user_params
-      render :new_populated
+      render "new_populated"
     else
       build_resource
-      render :new
+      render "new"
     end
   end
 
@@ -42,13 +42,28 @@ class Admin::InvitationsController < Devise::InvitationsController
     end
   end
 
-  # def create_partner
-  #   @partner = Partner.new(params[:partner])
-  #   if @partner.save
-  #     flash[:notice] = "Partner: #{@partner.name} has been created"
-  #   else
-  #     flash[:alert] = "Partner Not Saved"
-  #   end
-  #   redirect_to request.referer
-  # end
+  def new_professional
+    #TODO, testing if there are params in the URL, there needs to be a cleaner way.
+    if get_referer_params("invite_request[first_name]")
+      #TODO, add phone number to invite request and new professional form
+      @professional = Professional.new(name: get_referer_params("invite_request[first_name]").capitalize + " " +
+                                                         get_referer_params("invite_request[last_name]").capitalize,
+                                       email: get_referer_params("invite_request[email]"))
+      @professional.employments.build(role: get_referer_params("invite_request[role]"),
+                                      partner_id: params[:partner_id])
+    else
+      @employment = @partner.employments.build
+      @professional = @partner.professionals.build
+    end
+  end
+
+  def create_professional
+    @professional = Professional.new(params[:professional])
+    if @professional.save
+      render "create_professional"
+    else
+      render "new_professional"
+    end
+  end
+
 end
