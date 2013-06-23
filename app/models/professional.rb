dc5182a4f824518b645632c2ae552af5b6d76219
Sean <sean.t.irby@gmail.com>
2013-06-23 14:25:49 -0700

    Add validations and nested attributes writer

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -5,5 +5,9 @@ class Professional < ActiveRecord::Base
   has_many :students, :through => :mentorships
   has_many :mentorships
 
-  attr_accessible :email, :name, :phone_number
+  attr_accessible :email, :name, :phone_number, :employments_attributes
+
+  accepts_nested_attributes_for :employments
+
+  validates :email, :name, presence: true
 end
