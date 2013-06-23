72c0d7f2ad4b4ca91f24521d80f2954263747a90
Sean <sean.t.irby@gmail.com>
2013-06-23 14:33:33 -0700

    Add validations check in controller for invite_request model

--- app/controllers/invite_requests_controller.rb
+++ app/controllers/invite_requests_controller.rb
@@ -5,8 +5,14 @@ class InviteRequestsController < ApplicationController
 
   def create
     @invite_request = InviteRequest.new(params[:invite_request])
-    flash[:notice] = "An invitation request has been sent to the admin."
-    AdminMailer.invite_request(@invite_request).deliver
-    redirect_to new_user_session_path
+    if @invite_request.valid?
+      #Creating invite_request again because valid? adds weird errors parameter
+      @invite_request = InviteRequest.new(params[:invite_request])
+      flash[:notice] = "An invitation request has been sent to the admin."
+      AdminMailer.invite_request(@invite_request).deliver
+      redirect_to new_user_session_path
+    else
+      render :new
+    end
   end
 end
