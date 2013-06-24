2d5e598a1ea354060e4cf48d7e491e2bc7490971
Sean <sean.t.irby@gmail.com>
2013-06-23 20:33:48 -0700

    Edited factory so employment is created along with professional

--- spec/factories/partner_factory.rb
+++ spec/factories/partner_factory.rb
@@ -9,7 +9,8 @@ FactoryGirl.define do
         professional = create(:professional)
         #Need to define a helper method for below statement
         professional.update_attribute(:email, "#{professional.name.emailize}@#{partner.name.emailize}.com")
-        partner.professionals << professional
+        professional.employments.build(partner_id: partner.id, role: "Contact")
+        professional.save
       end
     end
   end
