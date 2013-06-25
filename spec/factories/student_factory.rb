adb47fe21d57f6fd7446473b96f98734d9d9e5fb
James Adney <jfadney@gmail.com>
2013-06-25 14:35:56 -0700

    add bio and links to student factory

--- spec/factories/student_factory.rb
+++ spec/factories/student_factory.rb
@@ -5,5 +5,7 @@ FactoryGirl.define do
     phone_number "(555) 555-5555"
     skills "javascript, c++, ruby"
     for_hire false
+    bio "Saved the world from alien invasion."
+    links "https://api.github.com/users/mojombo"
   end
 end
