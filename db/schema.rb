3446348a751431c6ed8e4460894cf5df229d71ae
Sean <sean.t.irby@gmail.com>
2013-06-25 15:04:03 -0700

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130625174023) do
+ActiveRecord::Schema.define(:version => 20130625214956) do
 
   create_table "courses", :force => true do |t|
     t.string   "title"
@@ -73,9 +73,9 @@ ActiveRecord::Schema.define(:version => 20130625174023) do
   create_table "relationships", :force => true do |t|
     t.integer  "partner_id"
     t.integer  "student_id"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.boolean  "connected",  :default => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+    t.boolean  "contact_allowed", :default => false
   end
 
   add_index "relationships", ["partner_id"], :name => "index_relationships_on_partner_id"
