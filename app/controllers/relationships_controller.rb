bd33dcfa16116c18e6992c2e67e294a48bf5f23f
Sean <sean.t.irby@gmail.com>
2013-06-24 18:20:19 -0700

    Add TODO in normal relationships controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,11 +2,13 @@ class RelationshipsController < ApplicationController
   before_filter :authenticate_user!
 
   #TODO: Use ajax?
-  #TODO: Add conditional to make sure a contact cannot be requestd twice
+  #TODO: Add conditional to make sure a contact cannot be requested twice
   def create
     Relationship.pending!(current_user.partner.id, params[:student_id])
     flash[:notice] = "You successfully requested contact with #{Student.find(params[:student_id]).name}."
     redirect_to students_path
   end
+
+  #TODO: Add destroy relationship
 
 end
