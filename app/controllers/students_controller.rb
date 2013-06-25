8c904e7f4c7204709e3f51a17698eb8600becc8d
Sean <sean.t.irby@gmail.com>
2013-06-25 13:34:37 -0700

    Add action to show user profile page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,13 @@
 class StudentsController < ApplicationController
   before_filter :authenticate_user!
+  #TODO: Add before filter to ensure user's partner has a relationship
 
   def index
     @students = Student.order("name")
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
 end
