6585901190b53aaf0347670c0ef2635e99c03f97
James Adney <jfadney@gmail.com>
2013-06-26 12:36:54 -0700

    setup email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,11 +1,11 @@
 class AdminMailer < ActionMailer::Base
-  default from: "admin_mailer@partnerdashboard.com"
+  default from: "no-reply@#{ENV["HOSTNAME"]}"
 
   def invite_request(invite_request)
     @invite_request = invite_request
     subject_suffix = "would like to be added to Partner Dashboard."
 
-    mail to: ADMIN_EMAIL,
+    mail to: ENV["ADMIN_EMAIL"],
          subject: "#{@invite_request.first_name} #{@invite_request.last_name} from #{@invite_request.company} #{subject_suffix}"
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,4 +71,14 @@ PartnerDashboard::Application.configure do
   config.action_mailer.default_url_options = {
     :host => ENV["HOSTNAME"]
   }
+
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 end
