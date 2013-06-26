673beb003b6dcc9a1b40819dfa5cef1737c0c8ba
James Adney <jfadney@gmail.com>
2013-06-25 19:24:48 -0700

    refactor unused method

--- app/controllers/invite_requests_controller.rb
+++ app/controllers/invite_requests_controller.rb
@@ -8,11 +8,17 @@ class InviteRequestsController < ApplicationController
     if @invite_request.valid?
       #Creating invite_request again because valid? adds weird errors parameter
       @invite_request = InviteRequest.new(params[:invite_request])
-      flash[:notice] = "An invitation request has been sent to the admin."
-      AdminMailer.invite_request(@invite_request).deliver
-      redirect_to new_user_session_path
+      send_invite_request(@invite_request)
     else
       render :new
     end
+  end
+
+  private
+
+  def send_invite_request(invite_request)
+      flash[:notice] = "An invitation request has been sent to the admin."
+      AdminMailer.invite_request(@invite_request).deliver
+      redirect_to new_user_session_path
   end
 end
