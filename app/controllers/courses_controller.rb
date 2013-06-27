089fe276452d00f292c0abeec6d8543d015f1ea6
Sean <sean.t.irby@gmail.com>
2013-06-26 18:13:09 -0700

    Add show action for course profile page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,9 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+
+  def show
+    @course = Course.find(params[:id])
+    @students = @course.students
+  end
 end
