29ba6b7fa62d1537538190b48a8d8230752db006
D Tucker <danielle@tuckerlabs.com>
2013-08-28 10:56:14 -0700

    Uncomment out view which depended on profile existing

--- spec/factories/professional_factory.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :professional do
-    first_names = ["Larry", "Lauren", "Michael", "Michelle"]
-    last_names = ["Laurence", "Michaels", "Mueller", "Smith", "Danger"]
-    name {"#{first_names.sample} #{last_names.sample}"}
-    phone_number  "555-555-5555"
-    email  {"#{name.emailize}@example.com"}
-  end
-end
