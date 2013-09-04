49aac786962dbbaecf57e0a28ba8b7696c5b37aa
D Tucker <danielle@tuckerlabs.com>
2013-09-04 14:40:11 -0700

    Closer reading of Postmark instructions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,5 @@ PartnerDashboard::Application.configure do
   }
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_KEY'] }
-
-  ActionMailer::Base.smtp_settings = {
-    :port           => '25',
-    :address        => ENV['POSTMARK_SMTP_SERVER'],
-    :user_name      => ENV['POSTMARK_API_KEY'],
-    :password       => ENV['POSTMARK_API_KEY'],
-    :domain         => 'ptr-dashboard.heroku.com',
-    :authentication => :plain
-  }
-  ActionMailer::Base.delivery_method = :smtp
 }
 end
