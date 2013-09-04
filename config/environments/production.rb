a3ca79862ada9acf4d716a766ae0eae437df361b
D Tucker <danielle@tuckerlabs.com>
2013-09-04 14:44:41 -0700

    Old typo found

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,5 +70,4 @@ PartnerDashboard::Application.configure do
   }
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_KEY'] }
-}
 end
