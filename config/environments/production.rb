cece975385e8efddcf4895980698fdc9343c8a20
natsteinmetz <nathalie.steinmetz@gmail.com>
2013-09-04 14:00:21 -0700

    Added prod. environment for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ PartnerDashboard::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = {
-    :host => ENV["HOSTNAME"]
+    :host => 'partner-dashboard-cf.herokuapp.com'
   }
 
   ActionMailer::Base.smtp_settings = {
